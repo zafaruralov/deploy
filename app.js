2b4623002ce8b79b0e7d6c26ca4aeb0a89698b71
zafaruralov <sultonovzafar007@gmail.com>
2022-08-20 22:35:00 +0500

    ready

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-/* eslint-disable no-unused-vars */
 const path = require('path');
 const fs = require('fs')
 
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 var multer = require('multer');
+var morgan = require('morgan')
+const helmet = require('helmet');
+require('dotenv').config()
  
 const Logger = require('./config/logger');
 const morganMiddleware = require('./config/morganMiddleware');
@@ -15,24 +16,22 @@ const middlewares = require('./middlewares');
 
 const authRoutes = require('./routes/auth.js');
 const productsRouter = require('./routes/product.js');
-const User = require('./models/user');
 const categoryRouter = require('./routes/category.js');
 const operatorRouter = require('./routes/operator.js');
 const courierRouter = require('./routes/courier.js');
 const accountantRouter = require('./routes/accountant.js');
 const searchRouter = require('./routes/search.js');
 const authUser = require('./middlewares/userType.js');
-var upload = multer({ dest: 'uploads/' });
+const swaggerUi = require('swagger-ui-express')
+let swaggerDocument = require('./swagger.json');
 
-const MONGODB_URI =
-  'mongodb+srv://zafaruralov:1A1s1D1f0@cluster0.rwu0b.mongodb.net/demoBro?retryWrites=true&w=majority';
+const MONGODB_URI = 'mongodb+srv://zafaruralov:1A1s1D1f0@cluster0.rwu0b.mongodb.net/demoBro?retryWrites=true&w=majority';
 
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'session'
 })
-
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -42,7 +41,6 @@ const fileStorage = multer.diskStorage({
     cb(null, new Date().toISOString() + '-' + file.originalname);
   }
 });
-
 
 const fileFilter = (req, file, cb) => {
   if (
@@ -55,13 +53,21 @@ const fileFilter = (req, file, cb) => {
     cb(null, false);
   }
 };
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  { flags: 'a' }
+);
+
 app.use(express.json()); 
-app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.urlencoded({ extended: false }));
 app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 );
 app.use('./images', express.static(path.join(__dirname, 'images')));
-
+app.use(helmet());
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(morganMiddleware);
 app.use(session({
@@ -70,7 +76,11 @@ app.use(session({
   saveUninitialized: false,
   store:store
 }))
-
+app.use(
+  '/',
+  swaggerUi.serve, 
+  swaggerUi.setup(swaggerDocument)
+);
 
 app.use(
   '/accountant',
@@ -111,20 +121,9 @@ app.use((err, req, res, next) => {
 
 app.use('*', middlewares.notFound);
 
-
 mongoose
   .connect(MONGODB_URI)
-  .then(result => {
-    User.findOne({ userType: 'superAdmin' }).then(user => {
-      if (!user) {
-        const user = new User({
-          email: 'superAdmin@gmail.com',
-          password: 'superadmin',
-          userType: 'superAdmin'
-        });
-        user.save();
-      }
-    });
+  .then(() => {
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Listening on port ${PORT}`),
 );
@@ -132,3 +131,4 @@ mongoose
   .catch(err => {
     console.log(err)
   });
+  
