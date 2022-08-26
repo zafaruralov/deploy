424e92a8d74dc472cea8f36f70e0c38738b91ae4
zafaruralov <sultonovzafar007@gmail.com>
2022-08-26 11:47:45 +0500

    make it better

--- routes/category.js
+++ routes/category.js
@@ -3,11 +3,10 @@ const express = require('express');
 const categoryRouter = express.Router()
 
 categoryRouter.route('/').get(categoryController.getAll)
-    .post(categoryController.createCategory)
+    
+categoryRouter.route('/create').post(categoryController.createCategory)
 
 categoryRouter.route('/:id')
     .get(categoryController.getOne)
-//     .put(categoryController.updateCategory)
-//     .delete(categoryController.deleteCategor)
 
 module.exports = categoryRouter
